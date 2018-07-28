acf1cad86ff5851472fb95fc7402ca3a668274bb
korfu92 <konrad.korf@gmail.com>
2018-07-28 11:49:24 +0200

    first commit

---
+++ WordSubstitution/FileOprtationsProvider.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WordSubstitution
+{
+     class FileOperationsProvider
+    {
+        [STAThread]
+        public static string GetFileContent()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                var path = ofd.FileName;
+                //Console.WriteLine("Ścieżka pliku: " + path); // full path
+
+                //Read file as string c#
+                string fileAsString = File.ReadAllText(path);
+                //Console.WriteLine("Plik jako string: " + fileAsString);
+                    
+                return fileAsString;
+            }
+            throw new Exception("You should've sselected a file, yo!");
+        }
+
+        [STAThread]
+        public static void SaveFile(string contents)
+        {
+            SaveFileDialog sFile = new SaveFileDialog();
+            if (sFile.ShowDialog() == DialogResult.OK)
+            {
+                var path = sFile.FileName;
+                System.IO.File.WriteAllText(path, contents);
+            }
+            Console.ReadKey();
+        }
+
+        public static JObject JsonConverter(string file)
+        {
+            JObject fileAsJSON = JObject.Parse(file);
+            Console.WriteLine("Plik jako JSON: " + fileAsJSON);
+            return fileAsJSON;
+        }
+
+        public static string ReplaceString(string config, JObject replacer)
+        {
+            foreach (var jToken in replacer)
+            {
+                config = config.Replace("{{"+jToken.Key+"}}", jToken.Value.ToString());
+            }
+            return config;
+        }
+
+   }
+}
--- WordSubstitution/Program.cs
+++ WordSubstitution/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
 namespace WordSubstitution
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            Console.WriteLine("Witaj wędrowcze!");
+
+            Console.WriteLine("Podaj plik konfiguracyjny: ");
+            var config = FileOperationsProvider.GetFileContent();
+           
+            Console.WriteLine("Podaj plik z JSONem: ");
+            var replace = FileOperationsProvider.GetFileContent();
+            var replaceJSON = FileOperationsProvider.JsonConverter(replace);
+
+            var result = FileOperationsProvider.ReplaceString(config, replaceJSON);
+            Console.WriteLine(result);
+
+            FileOperationsProvider.SaveFile(result);
+
+            Console.ReadKey();
         }
     }
 }
